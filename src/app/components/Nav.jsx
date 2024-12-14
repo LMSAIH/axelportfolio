5f247d68930f34b72c41a81287ba95bf96ab5dcd
Axel Velasquez <yoloax11@gmail.com>
2024-12-13 17:52:11 -0800

    created navbar component, to work on still

---
+++ src/app/components/Nav.jsx
@@ -0,0 +1,37 @@
+import Link from "next/link";
+
+const NavBar = () => {
+    return (
+
+        <nav>
+            <div className="navbar">
+
+                <div className="linkContainer">
+                    <div className="clickableContainer">
+                        <Link href="/" className="experience"> Experience </Link>
+                    </div>
+                    <div className="dotContainer">
+                        <div className="dot"></div>
+                    </div>
+
+                </div>
+
+                <div className="connectorContainer">
+                    <span className="connector"></span>
+                </div>
+
+                <div className="linkContainer">
+                    <div className="clickableContainer">
+                        <Link href="/" className="experience"> Experience </Link>
+                    </div>
+                    <div className="dotContainer">
+                        <div className="dot"></div>
+                    </div>
+                </div>
+            </div>
+        </nav>
+
+    );
+}
+
+export default NavBar;
