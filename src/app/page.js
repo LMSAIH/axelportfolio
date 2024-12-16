84193a4869ca4a2e709707cb94a9d92e994f2bd9
Axel Velasquez <yoloax11@gmail.com>
2024-12-15 17:39:53 -0800

    updated the page for testing purposes

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,20 @@
+"use client";
 import NavBar from "./components/Nav";
 
 export default function Home() {
   return (
     <div className="content">
       <NavBar />
-      <div className="left">
-        <p> Hello </p>
-      </div>
+
+      <main>
+        <div className="left">
+          <div className="introduction" id = "introduction"><p>hey</p> </div>
+          <div className="projects" id = "projects"><p>hey</p> </div>
+          <div className="skills" id ="skills"><p>hey</p> </div>
+          <div className="experience" id = "experience"><p>hey</p> </div>
+          <div className="contact" id ="contact"><p>hey</p> </div>
+        </div>
+      </main>
     </div>
   );
 }
