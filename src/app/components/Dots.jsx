002aa007d043d96e576657976de6893111e1f26d
Axel Velasquez <yoloax11@gmail.com>
2024-12-15 17:40:37 -0800

    created the dots elements, it also takes care of checking whether or not the user is toggling in an element to "lit' up the dot

---
+++ src/app/components/Dots.jsx
@@ -0,0 +1,44 @@
+import Link from "next/link";
+import { useEffect, useState } from 'react';
+
+
+const Dots = ({ name, targetRef }) => {
+
+    const [reached, setReached] = useState(false);
+
+    useEffect(() => {
+        const target = document.getElementById(targetRef);
+    
+        if (!target) return;
+    
+        const observer = new IntersectionObserver(
+          ([entry]) => {
+            setReached(entry.isIntersecting);
+          },
+          { threshold: 0.9 }
+        );
+    
+        observer.observe(target);
+    
+        return () => {
+          observer.unobserve(target);
+        };
+      }, [targetRef]);
+
+    return (
+        <div className="linkContainer">
+            <div className="clickableContainer">
+                <div>
+                    <Link href= {`#${targetRef}`} className = "experience">{name} </Link>
+                </div>
+            </div>
+            <div className="dotContainer">
+                <div className= {`dot ${reached ? "bg-blue-600" :"bg-white" } `}></div>
+            </div>
+
+        </div>
+
+    );
+}
+
+export default Dots;
