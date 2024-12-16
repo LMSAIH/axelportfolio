b6c9c36a0526ed9740114ddef6530dbc5cadbfe1
Axel Velasquez <yoloax11@gmail.com>
2024-12-15 17:40:00 -0800

    created a dot connector element

---
+++ src/app/components/DotConnector.jsx
@@ -0,0 +1,9 @@
+const DotConnector = () => {
+    return (
+        <div className="connectorContainer">
+            <span className="connector"></span>
+        </div>
+    );
+}
+
+export default DotConnector;
