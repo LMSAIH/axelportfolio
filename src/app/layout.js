8485d415678824ca53cbcf12091778cc6dbc274b
Axel Velasquez <yoloax11@gmail.com>
2024-12-15 17:39:38 -0800

    added a font to the layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,15 @@
 import "./globals.css"
+import { Roboto } from 'next/font/google';
+
+const roboto = Roboto({
+  subsets: ['latin'], 
+  weight: ['400', '500', '700'],
+});
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className={roboto.className}>
         {children}
       </body>
     </html>
