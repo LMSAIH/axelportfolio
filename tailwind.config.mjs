d74f018d16953592872a2704ef025e0a07a465a2
Axel Velasquez <yoloax11@gmail.com>
2024-12-17 09:26:09 -0800

    added the new color palette

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -8,8 +8,17 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: "#a33100",
+        secondary: "#b14a1f",
+        third: "#be6139",
+        fourth: "#ca7754",
+        fifth: "#d68d6e",
+        backgroundPrimary: "#121212",
+        backgroudSecondary: "#282828",
+        backgroundThird: "#3f3f3f",
+        backgroundFourth: "#575757",
+        backgroundFifth: "#8b8b8b",
+
       },
     },
   },
