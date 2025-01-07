b41398533e338e383e1141f87f3b2cf404ccf040
Axel Velasquez <yoloax11@gmail.com>
2025-01-06 18:17:51 -0600

    updated projects page to fit the layout

--- src/app/components/Projects.jsx
+++ src/app/components/Projects.jsx
@@ -11,7 +11,7 @@ const Projects = ({fonts}) => {
                 <h2 className={orbitron.className}> Take a look into some of <span>my work</span> </h2>
             </div>
 
-            <Project fonts = {fonts} source = "/sample.mp4" number={1}/>
+            <Project fonts = {fonts} source = "/sample.mp4" number={1} projectTitle={"ProfitSNFT"} projectContent={"This project was a special project in my heart, due"}/>
             <Project fonts = {fonts} source = "/sample.mp4" number={2}/>
             <Project fonts = {fonts} source = "/sample.mp4" number={3}/>
         </motion.div>
