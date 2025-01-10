165e6b18986d4c0859bd4053cd183ead035cb363
Axel Velasquez <yoloax11@gmail.com>
2025-01-09 20:43:39 -0800

    adjusted the page to the new skills component

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,7 @@ import {motion} from 'motion/react';
 import Introduction from "./components/Introduction";
 import Projects from "./components/Projects";
 import {Orbitron }from 'next/font/google';
+import Skills from "./components/Skills";
 
 const orbitron = Orbitron({
   subsets: ['latin'],
@@ -19,7 +20,7 @@ export default function Home() {
         <div className="left">
           <Introduction fonts={[orbitron]}/>
           <Projects fonts={[orbitron]}/>
-          <motion.div className="skills" id="skills"></motion.div>
+          <Skills fonts={[orbitron]} />
           <motion.div className="experience" id="experience"></motion.div>
           <motion.div className="contact" id="contact"></motion.div>
         </div>
