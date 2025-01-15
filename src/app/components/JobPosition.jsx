0d096e361e0e56532f92505a6c1ab16abc0a8ddc
Axel Velasquez <yoloax11@gmail.com>
2025-01-15 10:38:29 -0800

    finished job position component

--- src/app/components/JobPosition.jsx
+++ src/app/components/JobPosition.jsx
@@ -1,17 +1,31 @@
-const JobPosition = ({positionName, responsibilities, company, description, duration, fonts}) => {
+import { ArrowDownDoubleIcon } from "hugeicons-react";
+import { ArrowUpDoubleIcon } from "hugeicons-react";
+import { useState } from "react";
+
+const JobPosition = ({ positionName, responsibilities, company, description, duration, fonts }) => {
+
+    const [clicked, setClicked] = useState(false);
     const orbitron = fonts[0];
-    return ( 
+
+    return (
         <div className="jobPosition">
 
-            <h4>{positionName}</h4>
-            <p>{duration}</p>
-            <p> {company}</p>
-            <h5> Responsibilities </h5>
-            <p> {responsibilities}</p>
-            <h5> Role description </h5>
-            <p> {description}</p>
+            <div className="jobPositionTopContainer">
+                <h4 className={`positionName`}>{positionName}</h4>
+                {clicked ? <ArrowUpDoubleIcon onClick={() => {setClicked(!clicked)}} />: <ArrowDownDoubleIcon onClick={() => {setClicked(!clicked)}} /> }
+            </div>
+
+            <div className={`jobPositionBottomContainer ${   clicked ? "max-h-screen opacity-100" : "max-h-0 opacity-0"
+                } transition-all duration-500 overflow-hidden` }  >
+                <p className="positionDuration">{duration}</p>
+                <p className="positionCompanyName"> {company}</p>
+                <h5 className="positionResponsibilitiesTitle"> Responsibilities </h5>
+                <p className="positionResponsibilities"> {responsibilities}</p>
+                <h5 className="positionRoleDescriptionTitle"> Role description </h5>
+                <p className="positionRoleDescription"> {description}</p>
+            </div>
         </div>
-     );
+    );
 }
- 
+
 export default JobPosition;
