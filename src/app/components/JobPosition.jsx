53d9ae26d056938a0981534961d53f718a721d37
Axel Velasquez <yoloax11@gmail.com>
2025-01-18 21:19:40 -0800

    jobposition modified

--- src/app/components/JobPosition.jsx
+++ src/app/components/JobPosition.jsx
@@ -12,13 +12,13 @@ const JobPosition = ({ positionName, responsibilities, company, description, dur
 
             <div className="jobPositionTopContainer">
                 <h4 className={`positionName`}>{positionName}</h4>
-                {clicked ? <ArrowUpDoubleIcon onClick={() => {setClicked(!clicked)}} />: <ArrowDownDoubleIcon onClick={() => {setClicked(!clicked)}} /> }
+                {clicked ? <ArrowUpDoubleIcon onClick={() => { setClicked(!clicked) }} /> : <ArrowDownDoubleIcon onClick={() => { setClicked(!clicked) }} />}
             </div>
 
-            <div className={`jobPositionBottomContainer ${   clicked ? "max-h-screen opacity-100" : "max-h-0 opacity-0"
-                } transition-all duration-500 overflow-hidden` }  >
+            <div className={`jobPositionBottomContainer ${clicked ? "h-fit opacity-100" : "max-h-0 opacity-0"
+                } transition-all duration-500 overflow-hidden`}  >
+                <p className="positionDuration"> {company}</p>
                 <p className="positionDuration">{duration}</p>
-                <p className="positionCompanyName"> {company}</p>
                 <h5 className="positionResponsibilitiesTitle"> Responsibilities </h5>
                 <p className="positionResponsibilities"> {responsibilities}</p>
                 <h5 className="positionRoleDescriptionTitle"> Role description </h5>
