157bef0c46a77ed28049facc7880fc95311c6285
Axel Velasquez <yoloax11@gmail.com>
2025-01-18 21:19:12 -0800

    updated contact page

--- src/app/components/Contact.jsx
+++ src/app/components/Contact.jsx
@@ -1,23 +1,29 @@
 import { Mail01Icon } from "hugeicons-react";
+import { Github01Icon } from "hugeicons-react";
+import { DeveloperIcon } from "hugeicons-react";
 
-const Contact = ({fonts}) => {
+import Link from "next/link";
+
+const Contact = ({ fonts }) => {
 
     //TODO: add form
     const orbitron = fonts[0];
-    return ( 
+    return (
         <div className="contact" id="contact">
             <h3 className={`contactTitle ${orbitron.className}`}> Reach out </h3>
-            <div className = "socialsContainer">
-                <Mail01Icon /> 
+            <div className="socialsContainer">
+                <Link href="mailto:tarma792@gmail.com" passHref target="_blank"> <Mail01Icon />  </Link>
+                <Link href="https://github.com/LMSAIH" passHref target="_blank"> <Github01Icon />  </Link>
+                <Link href="https://devpost.com/yoloax11" passHref target="_blank"> <DeveloperIcon />  </Link>
             </div>
 
             <footer>
                 <div className="footer">
-                    <p className= "copyright"> &copy; Axel Velasquez {new Date().getFullYear()}. All rights reserved. </p>
+                    <p className="copyright"> &copy; Axel Velasquez {new Date().getFullYear()}. All rights reserved. </p>
                 </div>
             </footer>
         </div>
-     );
+    );
 }
- 
+
 export default Contact;
