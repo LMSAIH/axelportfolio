8b339c29a9e4e10a61e67c7ff5481aa149201de9
Axel Velasquez <yoloax11@gmail.com>
2025-01-20 10:31:04 -0800

    modified to account for metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,16 @@
-import "./globals.css"
-import { Roboto } from 'next/font/google';
+import "./globals.css";
+import { Roboto } from "next/font/google";
+
 
 const roboto = Roboto({
-  subsets: ['latin'], 
-  weight: ['400', '500', '700'],
+  subsets: ["latin"],
+  weight: ["400", "500", "700"],
 });
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={roboto.className}>
-        {children}
-      </body>
+      <body className={roboto.className}>{children}</body>
     </html>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,12 @@
 "use client";
 import NavBar from "./components/Nav";
-import {motion} from 'motion/react';
 import Introduction from "./components/Introduction";
 import Projects from "./components/Projects";
 import {Orbitron }from 'next/font/google';
 import Skills from "./components/Skills";
 import Experience from "./components/Experience";
 import Contact from "./components/Contact";
+import Head from "next/head";
 
 const orbitron = Orbitron({
   subsets: ['latin'],
@@ -15,7 +15,26 @@ const orbitron = Orbitron({
 
 export default function Home() {
   return (
+
+    
     <div className="content">
+
+      <Head>
+              <title>Axel Velasquez Portfolio</title>
+              <meta
+                name="description"
+                content="Welcome to Axel Velasquez's portfolio. I am a freelance web developer, and I would love to work with you!"
+              />
+              <meta name="viewport" content="width=device-width, initial-scale=1" />
+              <meta name="author" content="Axel Velasquez" />
+              <meta property="og:title" content="Axel Velasquez Portfolio" />
+              <meta
+                property="og:description"
+                content="Explore my work and discover how I can help bring your projects to life."
+              />
+              <meta property="og:type" content="website" />
+              <meta property="og:url" content="https://axelvelasquezportfolio.vercel.app" />
+            </Head>
       <NavBar />
 
       <main>
